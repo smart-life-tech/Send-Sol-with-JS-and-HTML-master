639b145ee4fa2dc358e14efdd7ca4082346dbce3
smart-life-tech <christlightworld@gmail.com>
2023-01-31 19:09:59 +0100

    new file:   index.html

--- connectWalletAndgetbalance.js
+++ connectWalletAndgetbalance.js
@@ -6,7 +6,7 @@ const app = new express();
 var fs = require('fs');
 
 http.createServer(function(req, res){
-    fs.readFile('index2.html',function (err, data){
+    fs.readFile('index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
